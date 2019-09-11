c3c64582d509a06e8f79ccfed8dcad75549e5faa
Edwin Kofler <24364012+eankeen@users.noreply.github.com>
2019-09-11 00:05:47 -0700

    polish: add comments to start script

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+# prune images and containers
 sudo docker image prune -f
 sudo docker container prune -f
 
+# build a Docker image as per the configuration files
 sudo docker image build --rm \
                         -fdockerfile \
                         -tmars_ek:default \
                         .
+
+# create the Docker container from the image and start it
 sudo docker run --rm \
                 --mount type=bind,src="$(cd "$(dirname "$0")" ; pwd -P)",dst=/home/developer,ro=false \
                 --mount type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix,ro=false \
